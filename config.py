e54e14fcc2c50aa19b73e24b7a228e0d4f9149b7
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-03-10 15:25:47 +0300

    all

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+class Config:
+    DEBUG = True
+    
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+    
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig
+    
+}
