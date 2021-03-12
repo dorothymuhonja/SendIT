d9f68eb97952c7c245311dfff05e81c0e1dba9ac
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-03-12 14:03:26 +0300

    all

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app.models import User, Parcel, Role
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
-app = create_app('productions')
+app = create_app('production')
 manager = Manager(app)
 manager.add_command('server', Server)
 
